44ee91d2cbe096c61fd25114d488102d91100a11
maiklzak <maiklzak@gmail.com>
2021-09-18 22:10:44 +0300

    crud memory, add MessageController

---
+++ src/main/java/com/zm/messaging_network/MessagingNetworkApplication.java
@@ -0,0 +1,13 @@
+package com.zm.messaging_network;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MessagingNetworkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MessagingNetworkApplication.class, args);
+	}
+
+}
