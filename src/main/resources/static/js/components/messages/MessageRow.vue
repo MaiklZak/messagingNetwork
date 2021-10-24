2e6cd6ded0cfc1a9fbf3aeb85b604d26d94ac144
maiklzak <maiklzak@gmail.com>
2021-10-24 12:55:16 +0300

    Open Graph Protocol: preview links on site

--- src/main/resources/static/js/components/messages/MessageRow.vue
+++ src/main/resources/static/js/components/messages/MessageRow.vue
@@ -4,6 +4,7 @@
       <i>({{ message.id }})</i>
       {{ message.text }}
     </v-card-text>
+    <media v-if="message.link" :message="message"></media>
     <v-card-actions>
       <v-btn value="Edit" @click="edit" small text rounded>Edit</v-btn>
       <v-btn icon @click="del" small><v-icon>mdi-delete-empty</v-icon></v-btn>
@@ -13,9 +14,11 @@
 
 <script>
 import { mapActions } from 'vuex'
+import Media from "components/media/Media.vue"
 
 export default {
   props: ['message', 'editMessage'],
+  components: {Media},
   methods: {
     ...mapActions(['removeMessageAction']),
     edit() {
